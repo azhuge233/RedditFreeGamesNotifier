152adb8afe7bfc9af090786b8936075f922a6608
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2024-05-04 13:59:42 +0800

    Add UA

--- RedditFreeGamesNotifier/Strings/ScrapeStrings.cs
+++ RedditFreeGamesNotifier/Strings/ScrapeStrings.cs
@@ -12,10 +12,12 @@
 		internal static string[] UAs = new string[] {
 			//"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
 			//"Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/112.0.0.0 Safari/537.36",
+			"RedditApp/614658 CFNetwork/1494.0.7 Darwin/23.4.0",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64",
 			"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:70.0) Gecko/20100101 Firefox/70.0",
 			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 OPR/65.0.3467.48",
-			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+			"Reddit/Version 2024.18.0/Build 614658/iOS Version 17.4.1 (Build 21E236)"
 		};
 
 		#region debug strings

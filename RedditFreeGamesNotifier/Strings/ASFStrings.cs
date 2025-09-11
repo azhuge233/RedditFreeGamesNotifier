3cc1ab3abe9f7992dc013c7aa51735d2f628fa92
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-09-11 18:22:55 +0800

    Add redeempoints command prefix in ASFStrings.

--- RedditFreeGamesNotifier/Strings/ASFStrings.cs
+++ RedditFreeGamesNotifier/Strings/ASFStrings.cs
@@ -3,6 +3,7 @@
 		internal static readonly string commandUrl = "{0}/Api/Command";
 
 		internal static readonly string addlicenseCommand = "addlicense asf ";
+		internal static readonly string redeemPointCommand = "redeempoints asf ";
 
 		internal static readonly string appIdRegex = @"app/[0-9]*";
 
